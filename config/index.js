e1eec1b76e13932350d9f384d496b844c7fa4d9b
wynwan <112957799+wynwan@users.noreply.github.com>
2022-09-07 17:07:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ export const config = {
       // 所在省份
       province: "天津",
       // 所在城市
-      city: "桥西区",
+      city: "天津",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
